2cebe542e6ee7eefbcc6a011eeb5c73ff0d0f677
Adolf Allotey <adolf.allotey@novastarcapital.fr>
2026-01-18 15:44:18 +0100

    ui: enable JSX parsing for eslint (babel parser)

--- src/v2/interface/react/eslint.config.js
+++ src/v2/interface/react/eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import babelParser from "@babel/eslint-parser";
 
 export default [
   { ignores: ["dist/**", "node_modules/**"] },
@@ -11,8 +12,16 @@ export default [
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
       globals: globals.browser,
     },
     plugins: {
@@ -20,10 +29,7 @@ export default [
       "react-refresh": reactRefresh,
     },
     rules: {
-      // React Hooks rules
       ...(reactHooks.configs?.recommended?.rules ?? {}),
-
-      // Vite + React refresh best practice
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     },
   },
